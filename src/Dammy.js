d5784ec8386be208d3e200d47abdbdfbf963ae56
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-01-06 20:44:55 +0530

    delete updated

---
+++ src/Dammy.js
@@ -0,0 +1,92 @@
+export const UserRows = [
+  {
+    id: 1,
+    username: "Thirumala",
+    avatar:
+      "https://lh3.googleusercontent.com/8gc9bTfDcH0qoeehDOzZ63t50zc4MKbGUCRQm6BaWPw6q31_8sJq0TnDVpDi30ODPCgnKJNGyLR-IfjvTzrli_offmcEmS1DrNgg3HoFJGOm1sRsVA64ZgIhzdf_ULHTmloUcO94gSCyy-XMvBm_XYbytiYvSDn7BPi2R-I1ajvvgMyBMuVj9M0fda1FciYqdsY9f-_sKjuq1Stk4Y-d1XOS9uB6Fug049NAelmhCwHgvPTWTAnDEPba529uNg3MEU9pWni-c_tgbS9h4XTha0mdLTJpsMHO2QKVSWLQ-aJ48bNgKibsahMQvw0e9gAUbRXLhfFYL6wjHS4bWChmrxUbqs6jsvVo6NQsU5-wLkPCADsA7Y0_dfIGuryvnqH1iGJ8qP32HiLVgEX5OiwvdY8f5vxuIJU-xSxsiH-8wnj0iqCJq-2zJEZ2Mpc_-VBnBiCrc-NIhDvkOXeeeZwjN0Y1q9vawf8Ojwp-g53Vb6-Iyi5X2JJro79HPwfvcuit52HqTT30V02IB6C3hEIjmaKhU8Elwz-pfvVU3SqAyccZ6a61M1chUkPRYMiHX9-6RzF8QiZaTFhcSSrrB-JnGGg-pl4C7nh8TF9BJB_dhz0Rqsd0TOI34zmL6fZoCBov8vBiBwemOmAq70wOQZJwT72RynzrdZe3GOhodEJtJ5FQ8p2HQ6GS2Pbs7S5fFPsOmu3GxLpF-uW2hb8RAmcxOvpl=w1194-h893-no?authuser=0",
+    email: "thiru@gmail.com",
+    status: "active",
+    transaction: "$6000.00",
+  },
+  {
+    id: 2,
+    username: "Sadheeshwaran U",
+    avatar:
+      "https://lh3.googleusercontent.com/_2TEbi2Qu4-Wo2bz8Bs9DTnC7P7AUDBxnMxJfGSJ3XB8ZcM9SxY5YbCsPWVItTB9bO5CnxyZRfovO0tU_Lr7nua9slo4dA9vZweXOulwsn3M1s5Uz5aMDextYPqFx_LLgdsRVm6AyAlq5fxQW009uFhq_-bf71E0POhs4lP0-4KpZBICaFKvGn5qv5s27df5_TCElTkC2xmcYI9LUT9l2k2aQNCisk1cj6lJ_XHp7lOIXLIrACsOCkux9AX7L2YouK4ykzr7us2Cvud4siy219B9R7_WIK6iYoWpWIMUBxaPlHy8V0REKe1CktVgU0a7BjXREZxheKbX8AXAcpxFyKbvC7_CwcbS9s1dpO34zXAwWn1LwGltTThJmP3F6iXEBKEzdNtZwqbt3daQPpi9cCI1n-OBSon5fCuq_FrnBxxjlY0NJoeV6BM4Ax3zLr745iu-reAVlPhXiIBE1MCaJtYWCzYyGapRWOeRIVMBrwtAIgvvKvFpZaN1evy7YYfPbhAk3I3Na1fBoks5T46JxWa2SGQPyqKuQaG3ZCeAkMhaRl6zft1MPLcLOdtZxGPfL7ynVxOHNq4ZlktJj4T9YoCsHghctj3V-bv5t5lNykO-FmS10OY31FmcJdjBcK_360h5iytQXp2g_LpZl4qgPxjX6Lc0-l6RGZh3JvoTkjk9yx99LozUe3HWU7oJ2BfSGQVjmP5H3QU6lRDUBqMr9Zbc=w670-h893-no?authuser=0",
+    email: "sadhe@gmail.com",
+    status: "active",
+    transaction: "$1000.00",
+  },
+  {
+    id: 3,
+    username: "Sanjith VG",
+    avatar:
+      "https://lh3.googleusercontent.com/58mer2Aq0GxILeYYNk0GqLTEhLg8OMu6K6aBkKdut5FiR6u5y4V1GTjZYusb_DpI2B6NqzDKxujUoL7Ea_T_lKvOUM8POQbSjZGHOlxKSohHhE0sLOaGZj1sMZV7njGsv_lya8JfITTsnuwf79yXkNuGhV7M-mzn4CrzA-no8sh45N-uWoxUBX2LZSdQAblgy7BFvrlROjBatH1lXNDflyLLbWofgXWA4uooPN_T6gYl_6mstpc_GZbDmg2Hl1Ik-YJTCxlSnJRo5RLPZ3FsGZeRjITURlBaPwb9vggk-jye2qKQvHkhgFgK4EM3sN8LTkqS9isuNjcDHuOwfF2WzfIMdAzdvBRoXb8vL6Ea4T2NEOe70YRMduvKoLL9exOib3DH9rRGZfIfDMVtCp0VhVQWLqNuZRUNI4OMt4mNFwAFSrxYX30x9izZNRK_qo_98Q_pwX22TCLK7sdcBlTbMAQ2YDXloWwfwTEBb68_rH36Wz_TuyCnmJjUeilA7_nbSkk9tjzdlLfao_Up7eCzfM8_xwFvBtD4cSEol4TA3HYbb_KcUjBiu8_LxpjaYzo43hUgJ0gUJW-yY_RTpgJ9dQq8oQzkITQy8K_CmDKKpohmyjzyYVW5gDWm7BvJrBAfcn6k60xFyTXM8Vl1YnjCWcgC5qpJUS5QJb33dFed848MPaLvz9tAr6fjtvcHqQAXItd6pwFjXZC8D3qlLckUzczT=w669-h893-no?authuser=0",
+    email: "sanjithvg@gmail.com",
+    status: "active",
+    transaction: "$2000.00",
+  },
+  {
+    id: 4,
+    username: "Kannan",
+    avatar:
+      "https://i.pinimg.com/originals/02/f8/a9/02f8a95963bb7122bedfe59d407020eb.jpg",
+    email: "kannan@gmail.com",
+    status: "active",
+    transaction: "$500.00",
+  },
+  {
+    id: 5,
+    username: "Sakthi",
+    avatar:
+      "https://static01.nyt.com/images/2017/11/02/arts/03thor-webmain/03thor-webmain-facebookJumbo.jpg?year=2017&h=549&w=1050&s=84faef933925f1c87f467e1a7fbcaab1d5b01bf5d0c19a7f7b92cb38c88965a5&k=ZQJBKqZ0VN",
+    email: "sakthi@gmail.com",
+    status: "active",
+    transaction: "$1500.00",
+  },
+  {
+    id: 6,
+    username: "Muththu",
+    avatar:
+      "https://images.unsplash.com/photo-1615789591457-74a63395c990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZG9tZXN0aWMlMjBjYXR8ZW58MHx8MHx8&w=1000&q=80",
+    email: "muththu@gmail.com",
+    status: "active",
+    transaction: "$4000.00",
+  },
+  {
+    id: 7,
+    username: "Karthik",
+    avatar:
+      "https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    email: "karthik@gmail.com",
+    status: "active",
+    transaction: "$2000.00",
+  },
+  {
+    id: 8,
+    username: "Ghousigan",
+    avatar:
+      "https://bd.gaadicdn.com/processedimages//640X309/5fd45bb1689f8.jpg",
+    email: "ghousi@gmail.com",
+    status: "active",
+    transaction: "$3000.00",
+  },
+  {
+    id: 9,
+    username: "GokulRaj",
+    avatar:
+      "https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d35eacaf1176b0008974b54%2F0x0.jpg%3FcropX1%3D790%26cropX2%3D5350%26cropY1%3D784%26cropY2%3D3349",
+    email: "gokul@gmail.com",
+    status: "active",
+    transaction: "$5000.00",
+  },
+  {
+    id: 10,
+    username: "Naveen",
+    avatar:
+      "https://www.pinkvilla.com/imageresize/ajith_kumar_thala_61.jpg?width=752&format=webp&t=pvorg",
+    email: "naveen@gmail.com",
+    status: "active",
+    transaction: "$2500.00",
+  },
+];
